b73b57f7e5920687da7b4d34ff6128fc7460706f
NimnaKs <nimnakaveesha123456@gmail.com>
2024-06-23 21:40:56 +0530

    user post method done

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,10 +1,20 @@
 import express,{Request,Response} from 'express';
-const router = express.Router();
+import UserModel from "../model/UserModel";
+const userRouter = express.Router();
 
 //create user
-router.post("/users", (req:Request, res:Response) => {
+userRouter.post("/users",async (req:Request, res:Response) => {
+  const {username,email,password,phone} = req.body;
+  const user = new UserModel();
+  user.username = username;
+  user.email = email;
+  user.password = password;
+  user.phone = phone;
+
+  await user.save();
+
   res.send("User Created.");
 });
 
 //export
-export default router;
+export default userRouter;
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,20 @@
 import express,{Request,Response} from 'express';
+import userRouter from './controllers/userController';
+import bodyParser from 'body-parser';
+import mongoose from "mongoose";
 
 const app = express();
 const port = 8000;
 
+app.use(bodyParser.json());
+
 app.get("/", (req:Request, res:Response) => {
   res.send('Application is running.');
 });
+
+mongoose.connect('mongodb://localhost:27017/sampleDB');
+
+app.use("/api",userRouter);
 
 app.listen(port, () => {
   console.log(`Application is running on port ${port}`);
